56b0db44d6bc7d8192406b23aa5b3a71b21c4ec6
John Oro <johnmichaeloro@gmail.com>
2019-04-02 11:00:00 -0600

    metrics and buttons

--- js/app.js
+++ js/app.js
@@ -31,6 +31,55 @@ const tomagotchiFactory = {
   }
 }
 
+$('#name-button').on('click', (e) => {
+  tomagotchiFactory.breedTomagotchi();
+  tomagotchiFactory.tomagotchis[0].name = $('.name-box').val();
+  console.log(tomagotchiFactory.tomagotchis[0]);
+  $('.name-box').val('');
+  $('#name-metric').text('Name: ' + tomagotchiFactory.tomagotchis[0].name);
+  setInterval(function() {
+    tomagotchiFactory.tomagotchis[0].hunger += 1;
+    $('#hunger-metric').text('Hunger: ' + tomagotchiFactory.tomagotchis[0].hunger);
+  }, 4000);
+  setInterval(function() {
+    tomagotchiFactory.tomagotchis[0].boredom += 1;
+    $('#boredom-metric').text('Boredom: ' + tomagotchiFactory.tomagotchis[0].boredom);
+  }, 3000);
+  setInterval(function() {
+    tomagotchiFactory.tomagotchis[0].sleepiness += 1;
+    $('#sleepiness-metric').text('Sleepiness: ' + tomagotchiFactory.tomagotchis[0].sleepiness);
+  }, 5000);
+  setInterval(function() {
+    tomagotchiFactory.tomagotchis[0].age += 1;
+    $('#age-metric').text('age: ' + tomagotchiFactory.tomagotchis[0].age);
+  }, 6000);
+})
+
+//I will probably have to use clearinterval when the marmot dies
+
+$('#feed-button').on('click', (e) => {
+  tomagotchiFactory.tomagotchis[0].hunger -= 1;
+  $('#hunger-metric').text('Hunger: ' + tomagotchiFactory.tomagotchis[0].hunger);
+})
+
+$('#play-button').on('click', (e) => {
+  tomagotchiFactory.tomagotchis[0].boredom -= 1;
+  $('#boredom-metric').text('Boredom: ' + tomagotchiFactory.tomagotchis[0].boredom);
+})
+
+$('#sleep-button').on('click', (e) => {
+  tomagotchiFactory.tomagotchis[0].sleepiness -= 1;
+  $('#sleepiness-metric').text('Sleepiness: ' + tomagotchiFactory.tomagotchis[0].sleepiness);
+})
+
+//Once the tomagotchi is instantiated, the timer starts.
+//The metrics change as the time increases.\
+//Pushing the buttons changes the metrics.
+//The metrics change the tomagotchi: it gets older and changes as it does. It can also die.
+//At a certain time, it dies.
+//Finally, animate the tomagotchi and finish styling the page.
+/**
 tomagotchiFactory.breedTomagotchi("Curlin");
 console.log(tomagotchiFactory.findTomagotchi(0));
 $('body').append(tomagotchiFactory.tomagotchis[0])
+**/
